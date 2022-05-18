af0c797b62de4bd6182411fcdfc1060c36ec65c7
atumitakahitoo <99326770+atumitakahitoo@users.noreply.github.com>
2022-05-18 20:03:34 +0900

    hey

--- main.py
+++ main.py
@@ -10,21 +10,21 @@ QR_CODE_COLOR = (80, 20, 80, 255)
 BACKGROUND_COLOR = (250, 250, 250, 255)
 
 st.title('QRコード作成アプリ')
-st.subheader('You can generate a QR code from a string or URL.')
+st.subheader('文字列やURLからQRコードを生成することができます。')
 st.text('PyQRCodeNG version')
-qr_url = st.text_input('Enter a string or URL to generate a QR code:', value='https://code2create.club/')
+qr_url = st.text_input('QRコードを読み込んだ際に表示される文字列やURLを入力しましょう:', value='https://code2create.club/')
 
 col1, col2 = st.columns(2)
 with col1:
-    qr_version = st.slider('Version (1-10)', 1, 10, value=5)
+    qr_version = st.slider('バージョン(QRコードの大きさの設定) (1-10)', 1, 10, value=5)
     qr_correction = st.select_slider(
-                        "Error Correction Grade: [L, M, Q, H]",
+                        "誤り訂正機能(汚れや破損などがあるとき、コードが自身で復元する能力、その階級): [L, M, Q, H]",
                         options=['L', 'M', 'Q', 'H'], value='H')
-    qr_scale = st.slider('Scale (4-8 pixels/cell)', 2, 8, value=4)
-    st.text(f'Length: {len(qr_url)}')
-    st.text(f'QR Code Version: {qr_version}')
-    st.text(f'Error Correction Grade: {qr_correction}')
-    st.text(f'Mode: Scale: {qr_scale}')
+    qr_scale = st.slider('スケール(QRコードを表示する大きさ) (4-8 ピクセル/セル)', 2, 8, value=4)
+    st.text(f'長さ: {len(qr_url)}')
+    st.text(f'QRコードのバージョン: {qr_version}')
+    st.text(f'誤り訂正機能: {qr_correction}')
+    st.text(f'スケール: {qr_scale}')
 
 try:
     qr = pyqrcode.create(qr_url, error=qr_correction, version=qr_version, mode='binary')
